affa0b759184d3ad00ded10fc06bc41722276dca
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-01 23:55:30 +0100

    refactored with oop

--- backend/cli/PageManager.ts
+++ backend/cli/PageManager.ts
@@ -2,12 +2,24 @@ import * as tools from './tools';
 
 export class PageManager {
 	private idCode = '';
+	private fileName = '';
 
-	constructor(idCode:string) {
+	constructor(idCode: string) {
+		if (!idCode) {
+			throw Error("invalid idCode");
+		}
+
 		this.idCode = tools.capitalizeFirstCharacter(idCode);
+		this.fileName = `Page${idCode}.tsx`;
 	}
 
-	public getIdCode() {
-		return this.idCode;
+	public createPage() {
+		tools.createFile(`src/pages/${this.fileName}`, `
+export const Page${this.idCode} = () => {
+	return (
+		<p>This is the ${this.idCode} page.</p>
+	)
+}
+`);
 	}
 }
--- backend/cli/cp.ts
+++ backend/cli/cp.ts
@@ -3,18 +3,15 @@ import * as tools from "./tools";
 
 const pageIdCode = tools.getArgument();
 
-const pageManager = new PageManager(pageIdCode);
-console.log(pageManager.getIdCode());
+try {
+	const pageManager = new PageManager(pageIdCode);
+	pageManager.createPage();
+}
+catch (e) {
+	console.log(`
+Please specify a page name in pascal notation.
+e.g. "npm run cp QuarterlyReports"
+`.trim());
+}
 
-// const pageFileName = `Page${pageIdCode}.tsx`;
-
-// tools.createFile(`src/pages/${pageFileName}`,
-// 	`
-// export const Page${pageIdCode} = () => {
-// 	return (
-// 		<p>This is the ${pageIdCode} page.</p>
-// 	)
-// }
-// 	`
-// );
 
