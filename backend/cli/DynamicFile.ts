b00588ca0f822ebe03a5cf6c91c074108ebe00fc
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-02 01:04:37 +0100

    nav tests ok

--- backend/cli/DynamicFile.ts
+++ backend/cli/DynamicFile.ts
@@ -15,12 +15,30 @@ export class DynamicFile {
 		this.lines = tools.convertStringBlockToLines(this.content);
 	}
 
+	public addStringBlockBeforeMarker(marker: string, stringBlock: string) {
+		const stringBlockLines = tools.convertStringBlockToLines(stringBlock);
+		const newLines: string[] = [];
+		for (const line of this.lines) {
+			if (line.includes(marker)) {
+				for (const stringBlockLine of stringBlockLines) {
+					newLines.push(stringBlockLine);
+				}
+			}
+			newLines.push(line);
+		}
+		this.lines = newLines;
+	}
+
+	private getContentFromLines() {
+		return this.lines.join('\n');
+	}
+
 	public debug() {
 		return `
 FILENAME: ${this.pathAndFileName}
 Number of lines: ${this.lines.length}
 ======================================
-${this.content}
+${this.getContentFromLines()}
 ======================================`.trim();
 	}
 }
--- backend/cli/debug.ts
+++ backend/cli/debug.ts
@@ -1,4 +1,7 @@
 import { DynamicFile } from "./DynamicFile";
 
 const dynamicFile = new DynamicFile('src/components/Nav.tsx');
+const marker = '</ul>';
+const stringBlockToAdd = `\t\t\t<li><NavLink to="/test002">Test002</NavLink></li>`;
+dynamicFile.addStringBlockBeforeMarker(marker, stringBlockToAdd);
 console.log(dynamicFile.debug());
